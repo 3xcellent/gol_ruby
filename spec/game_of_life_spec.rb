306eb27de7de3f87f28f71305d7fce761633f9da
Jason Gregory <magnet-dev+jg@asynchrony.com>
2015-10-20 00:10:27 -0500

    removes game rules text

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,20 +1,3 @@
-# The universe of the Game of Life
-#  - is an infinite two-dimensional orthogonal grid of square cells
-#  - each of which is in one of two possible states, alive or dead.
-#  -  Every cell interacts with its eight neighbours,
-#       which are the cells that are horizontally,
-#       vertically, or diagonally adjacent.
-#
-# At each _step_ in time, the following transitions occur:
-#  1 Any live cell with fewer than two live neighbours
-#      dies, as if caused by under-population.
-#  2 Any live cell with two or three live neighbours
-#      lives on to the next generation.
-#  3 Any live cell with more than three live neighbours
-#      dies, as if by overcrowding.
-#  4 Any dead cell with exactly three live neighbours
-#      becomes a live cell, as if by reproduction.
-#
 require_relative '../game_of_life'
 
 describe GameOfLife do
@@ -31,4 +14,5 @@ describe GameOfLife do
       expect(subject.width).to be width
     end
   end
+
 end
